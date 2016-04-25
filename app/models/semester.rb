453b2e04bab609928c6e58b1d1a408470f7839dd
Nerissa Lin <lin.nerissa@berkeley.edu>
2016-04-24 17:06:28 -0700

    i did a thing

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,5 +1,5 @@
 class Semester < ActiveRecord::Base
-	# validates :year, presence :true
-	# validates :season, presence :true
+	validates :year, presence :true
+	validates :season, presence :true
 	has_many :teams
 end
